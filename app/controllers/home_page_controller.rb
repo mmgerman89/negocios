426c8404e9faa1453bde10fdbceb686c128e4f2c
Germán Martínez <mmgerman89@gmail.com>
2018-04-18 15:31:09 -0300

    ajustes

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,5 +2,6 @@ class HomePageController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
     @plans = TypePlan.all.where(active: true)
+    @current_user_plan = helpers.current_plan(current_user)
   end
 end
--- app/helpers/user_plans_helper.rb
+++ app/helpers/user_plans_helper.rb
@@ -1,2 +1,7 @@
 module UserPlansHelper
+  def current_plan(user)
+    if user
+      user.user_plans.where("start_date <= '#{Date.current}' and end_date >= '#{Date.current}'").first
+    end
+  end
 end
